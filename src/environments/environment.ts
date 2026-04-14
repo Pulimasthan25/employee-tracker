780ca742be2852f5df8ab6e498732283f042313e
Pulimasthan25 <Pulimasthan25@gmail.com>
2026-04-14 23:13:46 +0530

    sfdgf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,4 @@
 export const environment = {
     production: false,
-    firebase: {
-      apiKey: "AIzaSyCAC9HHYKW3z3RVq33zfLb6Noer7FIGYLQ",
-authDomain: "employee-tracker-42c98.firebaseapp.com",
-projectId: "employee-tracker-42c98",
-storageBucket: "employee-tracker-42c98.firebasestorage.app",
-messagingSenderId: "201111663929",
-appId: "1:201111663929:web:8e26d2a11dd85234b3e4e8"
-    },
-    supabase: {
-      url: 'https://mgoxhdmmkaipkyjpxfow.supabase.co',
-      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nb3hoZG1ta2FpcGt5anB4Zm93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MDQ1ODMsImV4cCI6MjA4OTA4MDU4M30.xHtsthK4rAc-HnBP62UZtTpI4mC7OEb5XlNGya43kvs',
-      screenshotBucket: 'screenshots'
-    }
+
   };
